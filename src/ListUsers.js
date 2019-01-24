b59eb66674c9af44759638038ddc3b97aaf60eca
Naci Kalyoncu <naci.kalyoncu@gmail.com>
2019-01-24 21:15:25 +0000

    Exercise 1 - Passing Data

---
+++ src/ListUsers.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class ListUsers extends Component {
+    render() {
+        const users = this.props.users;
+        const profiles = this.props.profiles;
+        const movies = this.props.movies;
+
+        return (
+            <ul>
+                <h2>All Users and their Favorite Movies</h2>
+                {profiles.map(profile => {
+                    const userName = users[profile.userID].name;
+                    const movieName = movies[profile.favoriteMovieID].name;
+                    return (
+                        <li key={profile.id}>
+                            <p>{`${userName}\'s favorite movie is ${movieName}.`}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        )
+    }
+}
+
+export default ListUsers;
